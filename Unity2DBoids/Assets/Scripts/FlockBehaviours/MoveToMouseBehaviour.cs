8049a970bb708d72c40a0e1ee0604e3313f17fc1
frankvHoof93 <frank_van_hoof@hotmail.com>
2020-10-16 12:37:28 +0200

    [Finalized Implementation of required features] [TODO: Clean and improve]

--- Unity2DBoids/Assets/Scripts/FlockBehaviours/MoveToMouseBehaviour.cs
+++ Unity2DBoids/Assets/Scripts/FlockBehaviours/MoveToMouseBehaviour.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 [CreateAssetMenu(menuName = "Flock/Behaviours/Move To Mouse")]
 public class MoveToMouseBehaviour : AFlockBehaviour
@@ -10,17 +10,36 @@ public class MoveToMouseBehaviour : AFlockBehaviour
 
     public override Vector2 CalculateMove(Rocket agent, List<Transform> neighbours, FlockController flock)
     {
-        // TODO: Touch Input
-        if (Input.GetMouseButton(0))
+        // Check for Input
+        if (Input.GetMouseButton(0) || Input.touchCount > 0)
         {
-            Vector2 mousePos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            Vector2 offset = mousePos - (Vector2)agent.transform.position;
+            // Check if not using Canvas-UI
+            if (EventSystem.current.IsPointerOverGameObject())
+                return Vector2.zero; // Ignore Touch
+            Vector2? mousePos = null;
+            // Prioritise Touch
+            if (Input.touchCount > 0)
+            {
+                // Get WorldPos from Touch
+                Touch touch = Input.GetTouch(0);
+                mousePos = (Vector2)Camera.main.ScreenToWorldPoint(touch.position);
+            }
+            // FallBack to Mouse
+            else if (Input.GetMouseButton(0))
+            {
+                // Get WorldPos from Mouse
+                mousePos = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            // No Pos (Should not be possible here)
+            if (!mousePos.HasValue)
+                return Vector2.zero;
+            // Get Offset
+            Vector2 offset = mousePos.Value - (Vector2)agent.transform.position;
             float t = offset.magnitude / radius;
-
+            // Leniency
             if (t < 0.9f)
                 return Vector2.zero;
-
+            // Return Vector to move to
             return offset * (t * t);
         }
         else return Vector2.zero;
